6f3630a64f44818cfe6d3080ccfc7c3761d1169e
Sumiza <64884509+Sumiza@users.noreply.github.com>
2024-02-17 17:41:37 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
-# VERSION 1.032
+# VERSION 1.033
 ## URL https://raw.githubusercontent.com/Sumiza/micropython/main/update.py
 # URL https://raw.githubusercontent.com/Sumiza/micropython/beta/update.py
 # This is not recommended to use as it is a security risk
