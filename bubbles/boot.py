07b3b17d773db511876032faa9b4e3e98fa6e047
Sumiza <sumiza@gmail.com>
2024-03-08 02:04:23 -0500

    light test

--- bubbles/boot.py
+++ bubbles/boot.py
@@ -8,12 +8,12 @@
 
 from machine import Pin, reset
 
-pinswitch = dict()
+dipswitch = dict()
 
 for switch, pin in enumerate([10,11,12,13,14,15]):
-    pinswitch[switch+1] = Pin(pin,Pin.IN,Pin.PULL_DOWN)
+    dipswitch[switch+1] = Pin(pin,Pin.IN,Pin.PULL_DOWN)
 
-if pinswitch[1].value() == 1:
+if dipswitch[1].value() == 1:
     from wifi import Wifi
     import localdata
     wifi = Wifi(localdata.SSID,
@@ -27,19 +27,39 @@ if pinswitch[1].value() == 1:
         import settime
         settime.settime()
 
-    if pinswitch[2].value() == 1 and wifi.isconnected():
+    if dipswitch[2].value() == 1 and wifi.isconnected():
         import update
         update.updateall()
     
-    if pinswitch[6].value() == 1 and wifi.isconnected():
+    if dipswitch[6].value() == 1 and wifi.isconnected():
         import upload
         upload.run()
         reset()
 
-if pinswitch[3] == 1:
-    print('program starts here')
+if dipswitch[3].value() == 1:
 
+    import time
+    sensor = Pin(8, Pin.IN, Pin.PULL_DOWN)
+    presstime = time.ticks_ms()
+    counter = 0
 
-print('Done')
+    greenled  = Pin(9,Pin.OUT,value=0)
+
+    def bubble(pin):
+        global presstime
+        global counter
+        
+        if time.ticks_diff(time.ticks_ms(),presstime) > 1000:
+            presstime = time.ticks_ms()
+            counter += 1
+            greenled.on()
+            print(pin,presstime)
+
+    sensor.irq(trigger=Pin.IRQ_FALLING, handler=bubble)
+
+    while True:
+        time.sleep(1)
+        greenled.off()
+
     
 
