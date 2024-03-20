9beb8891daf43eec9f92859fc919601863be6913
Sumiza <sumiza@gmail.com>
2024-03-20 02:19:23 -0400

    common pinouts added

--- matrixkeypad.py
+++ matrixkeypad.py
@@ -1,7 +1,7 @@
-
-import asyncio
 import machine
 
+#pinout membrane r1 r2 r3 r4 c1 c2 c3 c4
+#pinout 3844 c1 c2 c3 c4 r1 r2 r3 r4
 keypad4x4 = {'pad':[
     [1,  2,  3,  'A'],
     [4,  5,  6,  'B'],
@@ -9,6 +9,10 @@ keypad4x4 = {'pad':[
     ['*',0,  '#','D']],
     'column':[3,2,1,0],
     'row':[7,6,5,4]}
+
+#pintout membrane r1 r2 r3 r4 c1 c2 c3
+#pinout 3845 c2 r1 c1 r4 c3 r3 r2
+#pinout 1824 r4 r3 r2 r1 c3 c2 c1
 keypad3x4 = {'pad':[
     [1,  2,  3],
     [4,  5,  6],
@@ -19,11 +23,10 @@ keypad3x4 = {'pad':[
 
 class MatrixKeypad():
     def __init__(self,padtype) -> None:
-        self.padtype = padtype
 
-        if self.padtype == '4x4':
+        if padtype == '4x4':
             self.keypad = keypad4x4
-        elif self.padtype == '3x4':
+        elif padtype == '3x4':
             self.keypad = keypad3x4
         else: ValueError('Keypad type needed 4x4 or 3x4')
         
@@ -50,20 +53,3 @@ class MatrixKeypad():
             col.value(0)
             if pushed is not None:
                 return pushed
-
-    async def scan_async(self):
-            pushed = None
-
-            for colnr, col in enumerate(self.col):
-                col:machine.Pin
-                col.value(1)
-
-                for rownr, row in enumerate(self.row):
-                    row:machine.Pin
-                    if row.value() == 1:
-                        pushed = self.keypad['pad'][rownr][colnr]
-                        break
-                    asyncio.sleep(0)
-                col.value(0)
-                if pushed is not None:
-                    return pushed
