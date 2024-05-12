e64f712c2db41b1d1c68ac5c69683a811ad0eb25
Sumiza <sumiza@gmail.com>
2024-05-11 20:07:39 -0400

    ntp added

--- boot.py
+++ boot.py
@@ -8,9 +8,9 @@ from machine import Pin, reset
 dipswitch = dict()
 
 for switch, pin in enumerate([10,11,12,13,14,15]):
-    dipswitch[switch+1] = Pin(pin,Pin.IN,Pin.PULL_DOWN)
+    dipswitch[switch+1] = Pin(pin,Pin.IN,Pin.PULL_UP) #pin to ground
 
-if dipswitch[1].value() == 1:
+if dipswitch[1].value() == 0:
     from wifi import Wifi
     import localdata
     wifi = Wifi(localdata.SSID,
@@ -23,20 +23,20 @@ if dipswitch[1].value() == 1:
     if wifi.isconnected():
         try:
             import settime
-            settime.settime()
+            settime.settime(5)
         except:
             pass # dont need time
 
-    if dipswitch[2].value() == 1 and wifi.isconnected():
+    if dipswitch[2].value() == 0 and wifi.isconnected():
         import update
         update.updateall()
     
-    if dipswitch[6].value() == 1 and wifi.isconnected():
+    if dipswitch[6].value() == 0 and wifi.isconnected():
         import upload
         upload.run()
         reset()
 
-if dipswitch[3].value() == 1:
+if dipswitch[3].value() == 0:
     ...
 
     
--- settime.py
+++ settime.py
@@ -1,29 +1,36 @@
-# VERSION 1.003
+# VERSION 1.004
 # URL https://raw.githubusercontent.com/Sumiza/micropython/main/settime.py
 
 import urequests
 import time
 from machine import RTC
 
-
-def settime(retry=1):
+def settime(retry=1,local=True,backup=False):
     resjson = None
-    for _ in range(retry):
-        try:
-            resjson = urequests.get('https://worldtimeapi.org/api/ip').json()
-        except Exception as e:
-            print('Download Error',e)
+
+    if local:
+        for _ in range(retry):
+            try:
+                resjson = urequests.get('https://worldtimeapi.org/api/ip').json()
+            except Exception as e:
+                print('Download Error',e)
+        
+        if resjson:
+            curtime = resjson['unixtime']+resjson['raw_offset']+resjson['dst_offset']
+
+            if time.gmtime(0)[0] == 2000:
+                curtime = curtime - 946684800
+            adjtime = time.gmtime(curtime)
+
+            datetimetuple = (adjtime[0],adjtime[1],adjtime[2],0,adjtime[3],adjtime[4],adjtime[5],0)
+            rtc = RTC()
+            rtc.datetime(datetimetuple)
+            print('Local time is:',time.localtime())
+            return True
     
-    if resjson:
-        curtime = resjson['unixtime']+resjson['raw_offset']+resjson['dst_offset']
-
-        if time.gmtime(0)[0] == 2000:
-            curtime = curtime - 946684800
-        adjtime = time.gmtime(curtime)
-
-        datetimetuple = (adjtime[0],adjtime[1],adjtime[2],0,adjtime[3],adjtime[4],adjtime[5],0)
-        rtc = RTC()
-        rtc.datetime(datetimetuple)
-        print('Local time is:',time.localtime())
+    if local is False or backup is True:
+        import ntptime
+        ntptime.settime()
         return True
+
     return False
